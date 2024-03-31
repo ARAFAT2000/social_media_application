48fc0e32d20cbb451edfad6de2e1070e507c522e
ARAFAT2000 <eee1916043brur@gmail.com>
2024-04-01 05:06:53 +0600

    user list complete

--- lib/view/user/user_list_screen.dart
+++ lib/view/user/user_list_screen.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 
 
@@ -9,8 +11,36 @@ class UserListScreen extends StatefulWidget {
 }
 
 class _UserListScreenState extends State<UserListScreen> {
+
+  DatabaseReference ref= FirebaseDatabase.instance.ref().child('User');
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('User List'),
+        elevation: 10,
+      ),
+      body: SafeArea(
+        child: FirebaseAnimatedList(
+            query: ref,
+            itemBuilder:(context,snapshot,animation,index){
+              return Card(
+                elevation: 0,
+                child: ListTile(
+                  //profile picture akhne upload krte hbe
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.blueGrey,
+                    child: Icon(Icons.person),
+                  ),
+                  title: Text(snapshot.child('username').value.toString()),
+                  subtitle: Text(snapshot.child('email').value.toString()),
+                ),
+              );
+            }
+        ),
+      ),
+    );
   }
 }
